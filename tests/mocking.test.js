b5a942d7813b2cce691c874bf023040b6671a077
Foursqweez <jeerasak.code@gmail.com>
2024-10-24 15:46:37 +0700

    Setup global Clearing mock before run each test

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -131,7 +131,7 @@ describe('signUp', () => {
   it('should send the welcome email if email is valid', async () => {
     await signUp(email);
 
-    expect(sendEmail).toHaveBeenCalled();
+    expect(sendEmail).toHaveBeenCalledOnce();
     const args = vi.mocked(sendEmail).mock.calls[0];
     expect(args[0]).toBe(email);
     expect(args[1]).toMatch(/welcome/i);
